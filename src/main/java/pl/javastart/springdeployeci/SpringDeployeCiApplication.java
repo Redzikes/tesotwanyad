475998b9063923ecb4cdd82748784651ac8afc7c
Redzikesxd <tomekreda16@gmail.com>
2018-06-17 15:42:39 +0200

    start

---
+++ src/main/java/pl/javastart/springdeployeci/SpringDeployeCiApplication.java
@@ -0,0 +1,12 @@
+package pl.javastart.springdeployeci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployeCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeployeCiApplication.class, args);
+    }
+}
